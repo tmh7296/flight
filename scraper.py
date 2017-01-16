bb6e25b35e611eafa193bd04046c3d76e8dfa3b7
tmh7296 <tmh7296@g.rit.edu>
2017-01-15 22:06:02 -0500

    giving it a shot

--- scraper.py
+++ scraper.py
@@ -1,7 +1,33 @@
-import requests, time, os, random
+import requests, time, os, random, re
 from bs4 import BeautifulSoup
 from twilio.rest import TwilioRestClient
 
+ORIGIN_AIRPORT = os.environ.get('ORIGIN_AIRPORT')
+DEST_AIRPORT = os.environ.get('DEST_AIRPORT')
+LEAVE_DATE = os.environ.get('LEAVE_DATE')
+RETURN_DATE = os.environ.get('RETURN_DATE')
+NUM_TICKETS = os.environ.get('NUM_TICKETS')
+MY_NUMBER = os.environ.get('MY_NUMBER')
+TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER')
+ACCOUNT_SID = os.environ.get('ACCOUNT_SID')
+AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
+
+def main():
+	outBoundSpirit, inBoundSpirit = scrapeSpirit()
+	outBoundSouthWest, inBoundSouthWest = scrapeSouthWest()
+	if (outBoundSpirit + inBoundSpirit) < (outBoundSouthWest + inBoundSouthWest):
+		lowestOutBoundFare = outBoundSpirit
+		lowestInBoundFare = inBoundSpirit
+		airline = "Spirit"
+	else:
+		lowestOutBoundFare = outBoundSouthWest
+		lowestInBoundFare = inBoundSouthWest
+		airline = "SouthWest"
+	if float(lowestOutBoundFare) < 150 or float(lowestInBoundFare) < 150:
+	message = "Cheapest outbound flight ("+ airline +"): $"+lowestOutBoundFare+ ", "\
+			"Cheapest inbound flight ("+ airline +"): $"+lowestInBoundFare+ 
+	twilio(message, MY_NUMBER)
+	
 def parseSouthWest(htmlText):
 	try:
 		print("parsing")
@@ -28,38 +54,95 @@ def parseSouthWest(htmlText):
 				pass
 		lowestOutBoundFare = (min(outBoundPrice))
 		lowestInBoundFare = (min(inBoundPrice))
-		if int(lowestOutBoundFare) < 150 or int(lowestInBoundFare) < 150:
-			message = "Cheapest outbound flight: $"+lowestOutBoundFare+ ", "\
-					"Cheapest inbound flight: $"+lowestInBoundFare
-			twilio(message)
+		return lowestOutBoundFare, lowestInBoundFare
 	except Exception as e:
 		message = e
-		print(message)
-		twilio("Something's fucked, man: " + message)
-	
-def twilio(message):
-	ACCOUNT_SID = os.environ.get('ACCOUNT_SID')
-	AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
+		twilio(message, "16105859087")
+		#twilio("Something's fucked, man: " + message)
+
+def parseSpirit(htmlText):
+	try:
+		page = BeautifulSoup(htmlText, 'html.parser')
+		chunks = page.find_all('div', {'class':'standardFare'})
+		flights = []
+		inBoundPrices = []
+		outBoundPrices = []
+		for el in chunks:
+			el = str(el)
+			if "Market1" in el:
+				price = ''
+				priceString = '<em class="emPrice">'
+				index = el.find(priceString) + len(priceString) + 1
+				while el[index].isdigit() or el[index] == ".":
+					price += el[index].strip("$")
+					index += 1
+				inBoundPrices.append(price)
+			elif "Market2" in el:
+				price = ''
+				priceString = '<em class="emPrice">'
+				index = el.find(priceString) + len(priceString) + 1
+				while el[index].isdigit() or el[index] == ".":
+					price += el[index].strip("$")
+					index += 1
+				outBoundPrices.append(price)
+		return min(outBoundPrices), min(inBoundPrices)
+	except Exception as e:
+		message = e
+		twilio(message, "16105859087")
+		
+def twilio(message, number):
 	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-	message = client.messages.create(to=os.environ.get('MY_NUMBER'),
-                                     from_=os.environ.get('TWILIO_NUMBER'),
+	message = client.messages.create(to=number, from_=TWILIO_NUMBER,
                                      body=message)
+
+def scrape(url, payload):
+	r = requests.post(url, data=payload)
+	return r.text
 
 def scrapeSouthWest():
 	payload = {
 		'returnAirport':'',
 		'twoWayTrip':'true',
 		'fareType':'DOLLARS',
-		'originAirport':os.environ.get('ORIGIN_AIRPORT'),
-		'destinationAirport':os.environ.get('DEST_AIRPORT'),
-		'outboundDateString':os.environ.get('LEAVE_DATE'),
-		'returnDateString':os.environ.get('RETURN_DATE'),
-		'adultPassengerCount':os.environ.get('NUM_TICKETS'),
+		'originAirport':ORIGIN_AIRPORT,
+		'destinationAirport':DEST_AIRPORT,
+		'outboundDateString':LEAVE_DATE,
+		'returnDateString':RETURN_DATE,
+		'adultPassengerCount':NUM_TICKETS,
 		'seniorPassengerCount':'0',
 		'promoCode':'',
 		'submitButton':'true'
 	}
-	r = requests.post("https://www.southwest.com/flight/search-flight.html", data=payload)
-	parseSouthWest(r.text)
+	url = "https://www.southwest.com/flight/search-flight.html"
+	html = scrape(url, payload)
+	return parseSouthWest(html)
+
+
+	
+def scrapeSpirit():
+    payload = {
+        'HiddenGuid':'907493d4-0e96-4d08-a623-87a5c8ca91b4',
+        'birthdates':'2/2/2004,6/16/2007',
+        'lapoption':'0,0',
+        'awardFSNumber':'',
+        'bookingType':'F',
+        'hotelOnlyInput':'',
+        'autoCompleteValueHidden':'',
+        'tripType':'roundTrip',
+        'from':ORIGIN_AIRPORT,
+        'to':DEST_AIRPORT,
+        'departDate':LEAVE_DATE,
+        'departDateDisplay':LEAVE_DATE,
+        'returnDate':RETURN_DATE,
+        'returnDateDisplay':RETURN_DATE,
+        'carPickUpTime':'16',
+        'carDropOffTime':'16',
+        'ADT':NUM_TICKETS,
+        'CHD':'2',
+        'INF':'0'
+    }
+	url = "https://www.spirit.com/Default.aspx?action=search"
+	html = scrape(url, payload)
+	return parseSpirit(html)
 
 scrapeSouthWest()
