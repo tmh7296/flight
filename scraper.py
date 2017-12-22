822c2da63c60695f26be3976a95356b52ae278a2
Tim <tmh7296@g.rit.edu>
2017-12-22 13:26:30 -0500

    trying this

--- scraper.py
+++ scraper.py
@@ -1,12 +1,41 @@
-import requests, time, os, random
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
+import requests, time, os, random
+
+
+originAirport = os.environ.get('ORIGIN_AIRPORT')
+destinationAirport = os.environ.get('DEST_AIRPORT')
+outboundDateString = os.environ.get('LEAVE_DATE')
+returnDateString = os.environ.get('RETURN_DATE')
+
+def flightPage():
+	driver = webdriver.Chrome()
+	driver.get("https://www.southwest.com/")
+	departure_airport = driver.find_element_by_id("air-city-departure")
+	arrival_airport = driver.find_element_by_id("air-city-arrival")
+	departure_airport.send_keys(originAirport)
+	arrival_airport.send_keys(destinationAirport)
+	arrival_date = WebDriverWait(driver,
+                                 10).until(EC.element_to_be_clickable((By.ID,
+                                                                       "air-date-return")))
+	departure_date = driver.find_element_by_id("air-date-departure")
+	departure_date.click()
+	departure_date.clear()
+	departure_date.send_keys(outboundDateString)
+	driver.find_element_by_tag_name("div").click()
+	arrival_date.click()
+	arrival_date.clear()
+	arrival_date.send_keys(returnDateString)
+	driver.find_element_by_id("jb-booking-form-submit-button").click()
+	parseSouthWest(driver.page_source)
 
 def parseSouthWest(htmlText):
 	try:
-		print("parsing")
 		page = BeautifulSoup(htmlText, 'html.parser')
-		print(str(page))
 		priceString = '<span class="currency_symbol">$</span>'
 		directionString = 'id="In'
 		flights = page.find_all('div', {'class':'product_info'})
@@ -29,7 +58,9 @@ def parseSouthWest(htmlText):
 				pass
 		lowestOutBoundFare = (min(outBoundPrice))
 		lowestInBoundFare = (min(inBoundPrice))
-		if int(lowestOutBoundFare) < 150 or int(lowestInBoundFare) < 150:
+		print("$" + str(lowestOutBoundFare))
+		print("$" + str(lowestInBoundFare))
+		if int(lowestOutBoundFare) < 1500 or int(lowestInBoundFare) < 1500:
 			message = "Cheapest outbound flight: $"+lowestOutBoundFare+ ", "\
 					"Cheapest inbound flight: $"+lowestInBoundFare
 			twilio(message)
@@ -37,30 +68,20 @@ def parseSouthWest(htmlText):
 		message = e
 		print(message)
 		twilio("Something's fucked, man: " + str(message))
-	
+
 def twilio(message):
 	ACCOUNT_SID = os.environ.get('ACCOUNT_SID')
 	AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
-	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-	message = client.messages.create(to=os.environ.get('MY_NUMBER'),
-                                     from_=os.environ.get('TWILIO_NUMBER'),
+	NUM = os.environ.get('MY_NUMBER')
+	TWILIO_NUM = os.environ.get('TWILIO_NUMBER')
+	client = Client(ACCOUNT_SID, AUTH_TOKEN)
+	message = client.messages.create(to=NUM,
+                                     from_=TWILIO_NUM,
                                      body=message)
 
-def scrapeSouthWest():
-	payload = {
-		'returnAirport':'',
-		'twoWayTrip':'true',
-		'fareType':'DOLLARS',
-		'originAirport':os.environ.get('ORIGIN_AIRPORT'),
-		'destinationAirport':os.environ.get('DEST_AIRPORT'),
-		'outboundDateString':os.environ.get('LEAVE_DATE'),
-		'returnDateString':os.environ.get('RETURN_DATE'),
-		'adultPassengerCount':os.environ.get('NUM_TICKETS'),
-		'seniorPassengerCount':'0',
-		'promoCode':'',
-		'submitButton':'true'
-	}
-	r = requests.post("https://www.southwest.com/flight/search-flight.html", data=payload)
-	parseSouthWest(r.text)
 
-scrapeSouthWest()
+flightPage()
+
+
+
+
